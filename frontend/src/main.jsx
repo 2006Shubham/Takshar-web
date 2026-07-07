2b8e0d67ec11f3d7331a5aab39754530470c218a
SSDLinux <shubhamdeshmukhs741@gmail.com>
2026-07-07 16:41:44 +0530

    fix:the  css style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css"
 
 import App from './App.jsx'
@@ -11,7 +11,7 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById("root")).render(
 
   <BrowserRouter>
-    <App/>
+    <App />
   </BrowserRouter>
 
 
